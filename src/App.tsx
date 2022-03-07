252bb08f4ca30673684093b9f1b25af10eea102f
Deramecourt Mathurin <contact@mderam.com>
2022-03-07 23:10:51 +0100

    feat: game create and join

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import './styles/App.css';
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
+import socketIOClient, { Socket } from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4000";
 
 const App: React.FC = () => {
     const [connectionStatus, setConnectionStatus] = useState<"connected" | "disconnected">("disconnected");
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [gameState, setGameState] = useState("");
+    const [joinGameId, setJoinGameId] = useState("");
+    const [createGameId, setCreateJoinGameId] = useState("");
 
     useEffect(() => {
-        const socket = socketIOClient(ENDPOINT, { transports: ["websocket"] });
-        socket.on("connect", () => {
+        const newSocket = socketIOClient(ENDPOINT, { transports: ["websocket"] });
+        setSocket(newSocket);
+        newSocket.on("connect", () => {
             setConnectionStatus("connected");
-            socket.emit("create game");
+            newSocket.emit("create game");
         });
-        socket.on("disconnect", () => {
+        newSocket.on("disconnect", () => {
             setConnectionStatus("disconnected");
         });
+        newSocket.on("game id", id => {
+            setCreateJoinGameId(id);
+        });
+        newSocket.on("game init", () => {
+            setGameState("init");
+        });
+        newSocket.on("error", error => {
+            if (error === "game not found") {
+                console.log(error);
+                setGameState("error");
+            }
+        });
 
-        return () => { socket.disconnect() };
+        return () => { newSocket.disconnect() };
     }, []);
 
+    function createGame() {
+        if (!socket) return;
+
+        socket.emit("game create");
+    }
+
+    function joinGame(id: string) {
+        if (!socket) return;
+
+        socket.emit("game join", id);
+    }
+
+    function isConnectionValid() {
+        return socket && connectionStatus === "connected";
+    }
+
     return (
-        <p>You are {connectionStatus}</p>
+        <div>
+            <p>You are {connectionStatus}</p>
+            {isConnectionValid() && <button onClick={createGame}>create game</button>}
+            {gameState === "error" && <p>An error occured</p>}
+            {socket &&
+                <>
+                <input
+                    id="gameId"
+                    name="gameId"
+                    type="text"
+                    value={joinGameId}
+                    onChange={e => setJoinGameId(e.target.value)}
+                />
+                <button onClick={() => joinGame(joinGameId)}>Join Game</button>
+                </>
+            }
+            {createGameId && <p>Share your link to play with a friend: {createGameId}</p>}
+            {gameState === "init" && <p>The game init</p>}
+        </div>
     );
 }
 
