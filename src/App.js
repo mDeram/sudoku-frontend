cb2d583c7fd3cad6df7daf7a7d5b18c2c40eeb24
Deramecourt Mathurin <contact@mderam.com>
2022-03-07 18:43:54 +0100

    basic backend ws connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import './styles/App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4000";
 
 function App() {
-  return (
-    <div lassName="App">
-          Learn React
-    </div>
-  );
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT, { transports: ["websocket"] });
+        socket.on("hello", data => {
+            setResponse(data);
+        });
+    }, []);
+
+    return (
+        <p>response: {response}</p>
+    );
 }
 
 export default App;
