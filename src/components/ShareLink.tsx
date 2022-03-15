c2d55e57bca7332a03e2f693ca3b7d624b513104
Deramecourt Mathurin <contact@mderam.com>
2022-03-15 20:16:32 +0100

    feat: share link copy feedback, copied or error

--- src/components/ShareLink.tsx
+++ src/components/ShareLink.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 interface ShareLinkProps {
     link: string;
 }
 
 const ShareLink: React.FC<ShareLinkProps> = ({ link }) => {
+    const initialCopyValue = "copy";
+    const [copy, setCopy] = useState<typeof initialCopyValue | "copied" | "error">(initialCopyValue);
+    const [copyTimeout, setCopyTimeout] = useState<any>(null);
+
+    function updateCopy(value: "copied" | "error") {
+        if (copyTimeout) clearTimeout(copyTimeout);
+
+        setCopy(value);
+        setCopyTimeout(setTimeout(() => setCopy(initialCopyValue), 1000));
+    }
+
     function copyLink() {
-        navigator.clipboard.writeText(link);
+        navigator.clipboard.writeText(link).then(
+            () => updateCopy("copied"),
+            () => updateCopy("error")
+        );
+    }
+
+    function getCopyClassName() {
+        switch(copy) {
+            case initialCopyValue: return "";
+            case "copied": return "successCopy";
+            case "error": return "errorCopy";
+        }
     }
 
     return (
@@ -12,7 +36,7 @@ const ShareLink: React.FC<ShareLinkProps> = ({ link }) => {
         <p>Share this link to invite players</p>
         <div className="shareLink">
             <input onFocus={e => e.target.select()} value={link} readOnly/>
-            <button onClick={copyLink}>copy</button>
+            <button onClick={copyLink} className={getCopyClassName()}>{copy}</button>
         </div>
         </>
     );
